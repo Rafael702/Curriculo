3cf3218cb46250e1be52bc086c18974a1f28fd96
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-05-22 20:54:56 -0300

    Inclusão do css

--- Aula 09.04/listar.php
+++ Aula 09.04/listar.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="listar.css">
     <title>Listar</title>
 </head>
 <body>
+    <h1><a href="incluir.php">Incluir Contato</a> </h1>
+
     
     <?php
     
@@ -14,20 +17,23 @@
 
     $contatos = mysqli_query($con, "Select * from  tb_contatos");
 
+    
+
     while($contato = mysqli_fetch_array($contatos)){
-        echo "<p class=\"box\">Nome $contato[nome]</p>";
-        echo "<p class=\"box\">Email $contato[email]</p>";
-        echo "<p class=\"box\">Telefone $contato[telefone]</p>";
-        echo "<p class=\"box\">Foto $contato[foto]</p>";
-        echo "<p class=\"box\">
+        echo "<div class=\"card\">";
+        echo "<p class=\"box importante\">Nome: $contato[nome]</p>";
+        echo "<p class=\"box\">Email: $contato[email]</p>";
+        echo "<p class=\"box\">Telefone: $contato[telefone]</p>";
+        echo "<p class=box>
         <img src=\"$contato[foto]\">
         </p>";
+        echo "</div>";
         
     }
 
 
     ?>
-
+    
 
 </body>
 </html>
